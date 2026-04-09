63ba010da2b1aad79f5404af0146c65a8d8610aa
hexter31376 <zwonyoung33@gmail.com>
2026-04-09 17:58:56 +0900

    initial commit

---
+++ src/main/java/com/jobhelper/careerflowapi/CareerflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.jobhelper.careerflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerflowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerflowApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jobhelper/careerflowapi/CareerflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobhelper.careerflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerflowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
